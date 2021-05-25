3c62108d443dee2fe8e244b1e0b92197a72ed446
wiktorssonn <56696783+wiktorssonn@users.noreply.github.com>
2021-05-25 14:21:13 +0200

    Skapat formulär där man fyller i önskad aktie samt kallar på andra komponenter som behöver datan från formuläret

---
+++ src/StockForm.js
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react';
+import StockApi from './StockApi';
+import FundamentalsTable from './FundamentalsTable';
+import Chart from './Chart';
+
+
+export default function StockForm() {
+    // Listor där data kommer sparas innan det presenteras i graf/tabell
+    const [fundamentals, setFundamentals] = useState([]);
+    const [graphLabels, setGraphLabels] = useState([]);
+    const [graphData, setGraphData] = useState([]);
+
+    // Variabler för att få data från formuläret
+    const inputTicker = useRef();
+    const selectTicker = useRef();
+
+    return (
+        <div className="container col-md-10">
+            <h4 className="mt-5">Ange ticker för att få ut data om aktien</h4>
+            <label for="ticker" className="form-label mb-0" >Ticker:</label>
+            <input type="text" className="form-control" name="ticker" placeholder="Eg. AAPL" ref={inputTicker}/>
+            
+            <label for="inspiration" className="form-label mt-3 mb-0">Ticker inspiration:</label>
+            <select className="form-select" ref={selectTicker}>
+                <option>Några vanliga tickers</option>
+                <option valute="AAPL">AAPL</option>
+                <option valute="AMZN">AMZN</option>
+                <option valute="BABA">BABA</option>
+                <option valute="GOOG">GOOG</option>
+                <option valute="MSFT">MSFT</option>
+                <option valute="TSLA">TSLA</option>
+                <option valute="WMT">WMT</option>
+
+            </select>
+
+            <p className="small">Om du anger en ticker i både textfältet och listan så är det tickern i textfältet som kommer användas.</p>
+
+            <StockApi fundamentals={fundamentals} setFundamentals={setFundamentals} inputTicker={inputTicker} selectTicker={selectTicker} graphData={graphData} graphLabels={graphLabels} setGraphData={setGraphData} setGraphLabels={setGraphLabels} />
+
+            <Chart graphData={graphData} graphLabels={graphLabels} />
+
+            <FundamentalsTable fundamentals={fundamentals} />
+
+        </div>
+    )
+}
